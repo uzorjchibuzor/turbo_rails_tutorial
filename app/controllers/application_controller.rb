41b1e88d8edb42a8786c22e6a51c98021c10d77a
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-08-03 09:16:28 +0100

    add secure broadcasts that only company members can see company posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
   
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,11 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+  belongs_to :company
 
   scope :ordered, -> { order(id: :desc) }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(quote) { [quote.company,"quotes"] }, inserts_by: :prepend
+
 end
