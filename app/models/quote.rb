1789e465f43d6ba56dfa1f073c68495b58c707b1
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-08-03 20:36:28 +0100

    create total component that dynamically updates any time there is changes to line items

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,11 +2,16 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   scope :ordered, -> { order(id: :desc) }
   # after_create_commit -> { broadcast_prepend_later_to "quotes" }
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   broadcasts_to ->(quote) { [quote.company,"quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 
 end
