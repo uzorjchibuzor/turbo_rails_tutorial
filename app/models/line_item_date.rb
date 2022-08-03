67fc12e5f28391642427ccce40b408c790c9aab4
Chibuzor Efedigbue <cefedigbuej@gmail.com>
2022-08-03 12:51:10 +0100

    add dates with association with company and quote add turbo frame and turbo stream for better UX add system test"

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -4,5 +4,9 @@ class LineItemDate < ApplicationRecord
   validates :date, presence: true, uniqueness: { scope: :quote_id }
 
   scope :ordered, -> { order(date: :asc) }
+
+  def previous_date
+    quote.line_item_dates.ordered.where("date < ?", date).last
+  end
   
 end
